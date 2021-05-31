6047ecb6ae70269dafa9aba18e868c519bad8e38
Thibaud Maurel <maurel.thibaud@free.fr>
2021-05-31 10:11:23 +0200

    Action Cable chat

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Chatroom.create(name: "general")
+User.create(nickname: 'alice', email: 'alice@gmail.com', password: '123456')
+User.create(nickname: 'bob', email: 'bob@gmail.com', password: '123456')
